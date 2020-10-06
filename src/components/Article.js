4e82d650d2bcac082eddc367ad4be98d72eede45
Kuzmenko Alexandr <akuzmenko@vodafone.ua>
2020-10-06 09:30:22 +0300

    last commit

--- src/components/Article.js
+++ src/components/Article.js
@@ -12,43 +12,36 @@ class Article extends Component {
     // 	return this.state.isOpen !== nextState.isOpen;
     // }
 
-    componentWillReceiveProps(nextProps) {
-    	console.log("---", "will receive props");
-    	if (nextProps.defaultOpen !== this.props.defaultOpen) {
-    		this.setState({isOpen: nextProps.defaultOpen});
-    	}
-    }
+    // componentWillReceiveProps(nextProps) {
+    // 	console.log("---", "will receive props");
+    // 	if (nextProps.defaultOpen !== this.props.defaultOpen) {
+    // 		this.setState({isOpen: nextProps.defaultOpen});
+    // 	}
+    // }
 
-    componentWillUpdate() {
-    	console.log("---", "will update");
-    }
+    // componentWillUpdate() {
+    // 	console.log("---", "will update");
+    // }
 
     render() {
-        const { article } = this.props;
+        const { article, isOpen, onButtonClick } = this.props;
         return (
             <div className="card mx-auto" style={{width: "70%"}}>
 	            <div className="card-header">
 		            <h2>
 						{ article.title }
-						<button className="btn btn-outline-secondary btn-lg float-right" onClick={ this.clickHandler }>
-							{this.state.isOpen ? "Hide" : "Open"}
+						<button className="btn btn-outline-secondary btn-lg float-right" onClick={ onButtonClick }>
+							{isOpen ? "Hide" : "Open"}
 						</button>
 						<hr />
 					</h2>
 	        	</div>
 				<div className="card-body">
 					<h6 className="card-subtitle text-muted">creation date: { (new Date(article.date)).toDateString() }</h6>	
-					{ this.state.isOpen  && <section className="card-text">{ article.text }</section> }
+					{ isOpen  && <section className="card-text">{ article.text }</section> }
 				</div>
 			</div>
         );
-    }
-
-    clickHandler = () => {
-        console.log("---", "clicked");
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
     }
 }
 
--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import Article from "../Article";
 import "./style.css";
 
-export default function ArticleList({ articles }) {
-    const articleElements = articles.map((article, index)  =>
-        <li className="article-list__li" key={ article.id }>
-        	<Article article={ article } defaultOpen={ index===0 } />
-        </li>
-    )
-    return (
-        <ul>
-			{ articleElements }
-		</ul>
-    );
+export default class ArticleList extends Component {
+    state = {
+        openArticleId: null
+    }
+
+    render() {
+        const articleElements = this.props.articles.map((article, index) =>
+            <li className="article-list__li" key={ article.id }>
+                <Article article={ article }
+                    isOpen={ this.state.openArticleId === article.id }
+                    onButtonClick={ this.clickHandler.bind(this, article.id) }
+                 />
+            </li>
+        )
+        return (
+            <ul>
+                { articleElements }
+            </ul>
+        )
+    }
+
+    clickHandler = (openArticleId) => this.setState({
+        openArticleId: this.state.openArticleId === openArticleId ? null : openArticleId
+    })
 }
